783dc4d3ee6cbc4b4240a4bbf8ff30b7f0f56722
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-12-19 18:16:18 +0100

    Add fields in form + Validations in Model Plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,12 @@
 class Plant < ApplicationRecord
+  AREAS = ['Europe', 'America', 'Africa', 'Asia', 'Oceania']
+  COLORS = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Violet', 'Purple']
   belongs_to :garden
 
   validates :name, presence: true, uniqueness: { scope: :garden_id }
   validates :image_url, presence: true
+  validates :area, inclusion: { in: AREAS }
+  validates :flower_color, inclusion: { in: COLORS }
+  validates :max_size, numericality: { greater_than: 0 }
+  validates :lifetime, numericality: { greater_than: 0 }
 end
