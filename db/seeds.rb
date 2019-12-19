54c0fb405fdd0c8e509a21790e957e7400bfd386
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-12-19 15:47:13 +0100

    Add some infos for a plant + Modify the Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
+puts "Cleaning database"
 Garden.destroy_all if Rails.env.development?
+puts "All gardens destroy"
+Plant.destroy_all if Rails.env.development?
+puts "All plants destroy"
 
 garden_names = [
   "French garden",
@@ -8,21 +12,52 @@ garden_names = [
   "Garden in Japan",
   "Walk among flowers"
 ]
+plant_area = [
+  "Europe",
+  "America",
+  "Africa",
+  "Asia",
+  "Oceania"
+]
+colors = [
+  "Red",
+  "Bleu",
+  "Green",
+  "Yellow",
+  "Orange",
+  "Violet",
+  "Purple"
+]
+
+puts "Create 6 gardens"
+g = 1
 6.times do
+  p = 1
   garden_request = RestClient.get("https://source.unsplash.com/1200x700/?garden")
   garden = Garden.new(
       name: garden_names.pop,
       banner_url: garden_request.request.url
     )
   garden.save!
+  puts "create garden #{g}"
+  g += 1
   3.times do
     plant_request = RestClient.get("https://source.unsplash.com/400x300/?flower")
     plant = Plant.new(
       name: Faker::FunnyName.name,
-      image_url: plant_request.request.url
+      image_url: plant_request.request.url,
+      category: Faker::FunnyName.four_word_name,
+      area: plant_area.sample,
+      fruit: [true, false].sample,
+      flower_color: colors.sample,
+      max_size: rand(1..25),
+      lifetime: rand(5..100)
     )
     plant.garden = garden
     plant.save!
-    sleep(2)
+    puts "create plant #{p}"
+    p += 1
   end
 end
+
+puts "Database Ready !"
