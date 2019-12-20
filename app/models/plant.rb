f1941702c4c6cd50d10a3b2d0aec6827d7207bb0
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-12-20 14:56:51 +0100

    Modify the plant form in garden show and plants controller

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -4,9 +4,8 @@ class Plant < ApplicationRecord
   belongs_to :garden
 
   validates :name, presence: true, uniqueness: { scope: :garden_id }
-  validates :image_url, presence: true
-  validates :area, inclusion: { in: AREAS }
-  validates :flower_color, inclusion: { in: COLORS }
-  validates :max_size, numericality: { greater_than: 0 }
-  validates :lifetime, numericality: { greater_than: 0 }
+  validates :image_url, :category, presence: true
+  validates :area, presence: true, inclusion: { in: AREAS }
+  validates :flower_color, presence: true, inclusion: { in: COLORS }
+  validates :max_size, :lifetime, presence: true, numericality: { greater_than: 0 }
 end
