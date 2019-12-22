05c887417addd5187fac3f96c3ecd64f538f0db0
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-12-22 10:07:41 +0100

    Active delete button in show plant page (with sweetAlert)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,19 @@
 import "bootstrap";
 import { animCreateGarden } from '../components/anim_create_garden';
+import { initSweetalert } from '../components/init_sweetalert';
+
 
 animCreateGarden();
+
+// Active sweetAlert before to delete an plant in show page
+initSweetalert('#delete-plant-btn', {
+  title: "Are you sure to want to delete this plant?",
+  text: "This action cannot be reversed",
+  icon: "warning",
+  buttons: true
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-plant');
+    link.click();
+  }
+});
